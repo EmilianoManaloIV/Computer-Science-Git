1854f388c5f845d2711f5779426d84320a71f909
EmilianoManaloIV <emilianomanaloiv@gmail.com>
2025-05-06 18:22:30 -0700

    Added selection and removal operations in deck class

--- Big2CSC-17C Project/Deck.h
+++ Big2CSC-17C Project/Deck.h
@@ -1,17 +1,19 @@
 #ifndef DECK_H
 #define DECK_H
 #include "Card.h"
+#include <list>
 #include <iostream>
 #include <deque>
 #include <map>
 #include <algorithm>
 #include <random>
+#include <vector>
 using namespace std;
 
 class Deck
 {
 private:
-    deque<Card> Cards;
+    deque<Card> Cards;              //Used to store all the card objects
     map<int, string> cardRankRef = { 
     {1, "3"},
     {2, "4"},
@@ -25,25 +27,32 @@ private:
     {10, "Q"},
     {11, "K"},
     {12, "A"},
-    {13, "2"}};
+    {13, "2"}}; //Map for card rank
     map<int, char> cardSuitesRef = 
     {{1, '&'},
     {2, '^'},
     {3, 'V'},
-    {4, 'O'}};
-    random_device rd;
+    {4, 'O'}};  //Map for suit rank
+    random_device rd;   //Random number generator using STL
     mt19937 g;
 public:
-    Deck();
-    ~Deck();
+    Deck();                         //Automatically creates a basic 52 hand without jokers
+    Deck(Card);                     //Creates a deck with one card in it
+    Deck(list<Card>);               //Creates a deck out of a given amount of cards
+    ~Deck();                        //Destroys the deck
     //---SPECIAL FUNCTIONS---
     void shuffleDeck();
     void sortDeck();
     void displayDeck();
+
     Card takeTopFromDeck();
     Card takeBottomFromDeck();
     Card takeRandomFromDeck();
+    Card takeCardFromDeck(int);
+    list<Card> selectCardsAndTakeFromDeck(list<int>);
     int amountOfCards();
+    void placeCardIntoDeck(Card);
+    void placeCardsIntoDeck(list<Card>);
 };
 //Creates a 52 deck without jokers at instantiation
 Deck::Deck() 
@@ -55,6 +64,16 @@ Deck::Deck()
             Cards.push_back(Card(j,i));
         }
     }
+}
+//Instantiates the deck with a single card specicifed
+Deck::Deck(Card C)
+{
+    Cards.push_back(C);
+}
+//Instantiates the deck with another set of cards
+Deck::Deck(list<Card> Cs)
+{
+    Cards = deque<Card>(Cs.begin(), Cs.end());
 }
 Deck::~Deck()
 {
@@ -109,9 +128,52 @@ Card Deck::takeRandomFromDeck()
 
     return C;
 }
+//Takes a certain card out of a specified index in the deck
+Card Deck::takeCardFromDeck(int I)
+{
+    Card C = Cards.at(I);
+    Cards.erase(Cards.begin() + I);
+
+    return C;
+}
 //Returns the amount of cards this deck has
 int Deck::amountOfCards()
 {
     return Cards.size();
+}
+//Places a card into the deck
+void Deck::placeCardIntoDeck(Card C)
+{
+    Cards.push_back(C);
+}
+//Places a list of cards into the deck
+void Deck::placeCardsIntoDeck(list<Card> Cs)
+{
+    Cards.insert(Cards.end(),Cs.begin(),Cs.end());
+}
+//Selects cards then take them out of the deck
+list<Card> Deck::selectCardsAndTakeFromDeck(list<int> selection)
+{
+    std::list<Card> temp;
+
+    // Convert selection of indices to actual Card objects
+    auto it = Cards.begin();
+    for (const auto& i : selection)
+    {
+        std::advance(it, i);  // Move iterator to the desired index
+        temp.push_back(*it);  // Add the selected card to temp
+        it = Cards.begin();  // Reset iterator to beginning for next selection
+    }
+
+    // Now remove selected cards from the original deck
+    for (const auto& card : temp)
+    {
+        auto removeIt = std::find(Cards.begin(), Cards.end(), card);
+        if (removeIt != Cards.end()) {
+            Cards.erase(removeIt);  // Remove the card from the deck
+        }
+    }
+
+    return temp;  // Return the list of selected cards
 }
 #endif 
